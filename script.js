bae24e2df3da63a848faffc9880c5f3a17fe92fa
Lanre Akinbo <116492716+lanre-akinbo@users.noreply.github.com>
2023-10-16 21:02:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function sendMail() {
+    var params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_20pigze", "template_jm79tde", params).then(function(res) {
+        alert("Success!" + res.status)
+    })
+}
